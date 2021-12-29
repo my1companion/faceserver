67b813138d51c81e69a8eefc5ea172a9a3ee8f0d
my1companion <my1companion@gmail.com>
2021-12-29 11:08:19 +0100

    DATABASE CONNECTION

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ app.use(cors());
 const db = knex({
 	client: 'pg',
 	connection: {
-		 connectionString: process.env.DATABASE_URL,
-	  ssl: {
-	    rejectUnauthorized: false
-	  }
+		connectionString: process.env.DATABASE_URL,
+	  	ssl: true
 	}
 })
 
+// {
+// 	    rejectUnauthorized: false
+// 	  }
 
 // host:'127.0.0.1',
 // 		user:'postgres',
