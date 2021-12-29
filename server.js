31e719629cbfbb93eb48a1ffe2ef0a3c31cf0855
my1companion <my1companion@gmail.com>
2021-12-29 11:18:59 +0100

    DATABASE CONNECTION

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 const app = express();
+const { Pool } = require('pg');
 app.use(bodyParser.json());
 app.use(cors());
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
-const db = knex({
-	client: 'pg',
-	connection: {
-		connectionString: process.env.DATABASE_URL,
-	  	ssl: true
-	}
-})
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+// const db = knex({
+// 	client: 'pg',
+// 	connection: {
+// 		connectionString: process.env.DATABASE_URL,
+// 	  	ssl: true
+// 	}
+// })
 
 // {
 // 	    rejectUnauthorized: false
