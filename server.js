36ecc0918135c25400dbf19d84f92c15ef1c0266
my1companion <my1companion@gmail.com>
2021-12-30 08:00:29 +0100

    database connection

--- server.js
+++ server.js
@@ -173,16 +173,12 @@ app.put('/image', (req, res) =>{
 
 	let found = false;
 
-				db('users').where('id','=',id)
-			.increment('entries',1)
-			.returning('entries')
-			.then(entries => {
-				res.json(entries[0];})
-				.catch(err=>{ res.status(400).json('unable to connect');});
 
 
 
-
+	if(!found){
+	    res.json('user found');
+}
 });
 
 
